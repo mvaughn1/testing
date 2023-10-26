cbb86b0d775eaccc1c077740808a359161ee70c9
Mark Vaughn <mvaughn1@gmail.com>
2023-10-25 17:36:08 -0700

    asdf

---
+++ fib.c
@@ -0,0 +1,35 @@
+ #include <stdio.h>
+//#include <stdlib.h>
+#include <strings.h>
+
+
+int Fib(int fcount){
+    double long Fm[fcount];
+    int count;
+    int i;
+    
+    Fm[0] = 0;
+    Fm[1] = 1;
+    
+    for (i = 2; i < fcount; i++){
+
+        Fm[i] = (Fm[i - 2]) + (Fm[i-1]);
+    }
+    //char buffIt[] = "test";
+   
+    printf("Here are %i Fibs:\n", fcount);
+    for (i = 0; i < fcount; i++) {
+        //itoa((int)Fm[i], buffIt, 10);
+        printf("Fib[%d] is %d \n", i, (int32_t) Fm[i]);
+    }
+    return 0 ;
+}
+int main(){
+    int totalFibs = 0;
+    printf("\nHow many Fibs?\n");
+    scanf("%i", &totalFibs);
+
+    Fib(totalFibs);
+
+return 0;
+}
--- mark.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-
-
-    printf("Hello world 2023");
-
-    return 0;
-}
