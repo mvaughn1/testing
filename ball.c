8c4c0363a79cb91a4a8458ac191563a348f07b14
Mark Vaughn <mvaughn1@gmail.com>
2023-10-26 21:40:12 -0700

    adding ball again

--- ball.c
+++ ball.c
@@ -2,14 +2,14 @@
 #include <stdlib.h>
 #include<unistd.h>
 
-
 int main(){
 
 // ball is z zero character and needs to appear to rise on the screen then fall...
 
-    system("cls");
+
     printf("         0    ");
     sleep(2);
+    system("clear");  
     printf("\n        -       ");
 
     return 0;
