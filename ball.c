067de91d3e7f90ec2b164ec809bd2008c5034f46
Mark Vaughn <mvaughn1@gmail.com>
2023-10-27 09:51:39 -0700

    baller

--- ball.c
+++ ball.c
@@ -3,25 +3,32 @@
 #include <unistd.h>
 #include <string.h>
 
+char mark1[10][10]; //, mark2[10], mark3[10], mark4[10];
+
 int main(){
 
-// ball is z zero character and needs to appear to rise on the screen then fall...
+    // ball is z zero character and needs to appear to rise on the screen then fall...
+    // let's try working with 3 lines, screen clear between the runs
+    int i = 0;
+    float yy = 1;
 
-int i = 0;
-float yy = 0.5;
- char mark1[4][10] = {"0---","-0--", "--0-", "---0"};
-system("clear");
-for (i = 0; i < 4; i++){
-    //put the ball in 4 positions...
+    strcpy(mark1[1], "0^^^");
+    strcpy(mark1[2], "_0^^");
+    strcpy(mark1[3], "__0^");
+    strcpy(mark1[4], "___0");
 
-    sleep(yy);
     system("clear");
-    fflush(stdout);
-    printf("\r            ");
-    printf("\%s   ", mark1[i]);
-    printf("\n           ");
-    
-}
+    for (i = 1; i < 5; i++){
+        //put the ball in 4 positions...
+
+        sleep(yy);
+        system("clear");
+        fflush(stdout);
+        printf("\r            ");
+        printf("\%s   ", mark1[i]);
+        printf("\n           ");
+        
+    }
 
 
     return 0;
