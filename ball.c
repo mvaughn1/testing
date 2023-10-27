9b35e12d229c0c13267bcf0c4bbad08d89464517
Mark Vaughn <mvaughn1@gmail.com>
2023-10-26 21:32:44 -0700

    adding ball

---
+++ ball.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+
+
+int main(){
+
+// ball is z zero character and needs to appear to rise on the screen then fall...
+
+    system("cls");
+    printf("         0    ");
+    sleep(2);
+    printf("\n        -       ");
+
+    return 0;
+}
